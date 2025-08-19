5efb8164e5deba27c3bfab5fdb986fbda6a39c98
Sambit Panigrahi <panigrahisambit1@gmail.com>
2025-08-19 17:22:21 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mulish: ["Mulish", "sans-serif"],
+    },
+     colors: {
+          deep: "#02042a",
+          deepBlue: "#0a1530",
+          light: "#2b84ea",
+          light300: "#4b94ed",
+          light500: "#0b72e7",
+          greenLight: "#61cea6",
+          grayText: "#818597",
+          lightGray: "#e2e2e2",
+          grayBlue: "#344a6c",
+          deepHead: "#162f56",
+          gray2: "#525a76",
+        },
+  },},
+  plugins: [],
+}
+
